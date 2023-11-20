c320d4b9027358a8e836521321c7fe3b2ccd61f3
roketrig <b1m.ksr@yahoo.com>
2023-11-20 23:27:45 +0300

    Add project files.

---
+++ Ortalama Hesaplama Patika.dev/Fibonacci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ortalama_Hesaplama_Patika.dev
+{
+    public class Fibonacci
+    {
+        public int OrtalamaHesapla(int deger)
+        {
+
+            int[] sayilar = new int[deger];
+            sayilar[0] = 1;
+            sayilar[1] = 1;
+            int toplam = 0;
+
+            for (int i = 2; i < deger; i++)
+            {
+                sayilar[i] = sayilar[i - 1] + sayilar[i - 2];
+                toplam += sayilar[i];
+            }
+            int b = toplam / deger;
+
+            
+
+            for (int i = 0; i < deger; i++)
+            {
+                Console.Write(sayilar[i]+ ", ");
+            }
+
+            Console.WriteLine("Fibonacci Ortalaması :" + b);
+
+            return deger;
+        }
+    }
+}
---
+++ Ortalama Hesaplama Patika.dev/Program.cs
@@ -0,0 +1,6 @@
+﻿using Ortalama_Hesaplama_Patika.dev;
+
+Console.Write("Lütfen Fibonacci değerini giriniz :");
+int a = Convert.ToInt32(Console.ReadLine());
+Fibonacci fibonacci = new Fibonacci();
+fibonacci.OrtalamaHesapla(a);
